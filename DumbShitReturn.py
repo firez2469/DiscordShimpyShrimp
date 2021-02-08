6d20323b249501f9ead2f4a8ae49cff4f63bd017
firez2469 <47616467+firez2469@users.noreply.github.com>
2021-02-07 19:16:44 -0500

    Add files via upload

--- DumbShitReturn.py
+++ DumbShitReturn.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # bot.py
 import os
 import random
@@ -46,3 +47,53 @@ async def dumb_shit_loop(ctx,loop=5,speech='False'):
 
 
 bot.run(TOKEN)
+=======
+# bot.py
+import os
+import random
+from discord.ext import commands
+import asyncio
+import random 
+
+bot = commands.Bot(command_prefix='.')
+counter = {}
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    
+    #await channel.send('**THE NUKES WILL SOON BE DROPPED**')
+    print(bot.guilds)
+
+
+
+@bot.command()
+async def dumb_shit(ctx,speech='False'):
+    isTTS = speech=='True'
+    channel = bot.get_channel(803112589156024371)
+    #channel = bot.get_channel(803349900036669490)
+    messages = await channel.history(limit=200).flatten()
+    msg = random.choice(messages)
+    
+    await ctx.send(msg.content,tts=isTTS)
+
+@commands.has_role('Admin')
+@bot.command()
+async def dumb_shit_loop(ctx,loop=5,speech='False'):
+    isTTS = speech=='True'
+    channel =bot.get_channel(803112589156024371)
+    messages = await channel.history(limit=200).flatten()
+    for av in range(loop):
+        msg = random.choice(messages)
+        await ctx.send(msg.content,tts=isTTS)
+
+
+#bot.loop.create_task(search_submissions())
+
+
+bot.run(TOKEN)
+>>>>>>> 9dc94976c8e5bcb6b93c117b9ee3dc6c8733e767
