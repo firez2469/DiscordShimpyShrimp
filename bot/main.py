ad0b0c031f73c43ba162ff1de2693c663ffeb698
DJSheep <74786558+DJSheep@users.noreply.github.com>
2021-04-16 15:58:15 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -167,9 +167,9 @@ async def dumb_shit_admin(ctx):
         title="Dumb Shit Moment", 
         description="Uh oh someone was stupid!",
         color=0xff0000)
-    embed.set_image(url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FDumbHoeMemes%2F&psig=AOvVaw3idA5n1eNyAy30cVIycO17&ust=1618685894972000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIj_m4C5g_ACFQAAAAAdAAAAABAE')
+    embed.set_image(url='https://media.tenor.com/images/9133bff595c13cd663e40f6b73ff1196/tenor.gif')
     embed.set_author(name='Mr. Gump',
-                     icon_url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FForrest_Gump_(character)&psig=AOvVaw1dzQyJYAtWZoLZuZBy2OWD&ust=1618688824521000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNDNtfXDg_ACFQAAAAAdAAAAABAE')
+                     icon_url='https://upload.wikimedia.org/wikipedia/en/9/94/Forest_Gump_Character.jpg')
     embed.add_field(name=(msg.author.mention + " has found " + sh.findAuthor(msg.content)), 
                     value="guilty of dumb shit!!", inline=False)
     embed.add_field(name=("Evidence:"),
